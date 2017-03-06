7c4db57bdb57a7745b93c0ac32eb06c8a6e8186a
neiljoshua <neiljoshua@me.com>
2017-03-05 22:17:27 -0500

    menu bug fixed

--- wp-content/themes/sanisidro/src/js/sanisidro.js
+++ wp-content/themes/sanisidro/src/js/sanisidro.js
@@ -1,6 +1,28 @@
 (function ($, root, undefined) {
 
-	$(document).ready(function(){
+$(document).ready(function(){
+
+	var $window = $(window);
+  var $header = $('.site-header');
+  var $nav = $('.site-nav');
+  var $mobile =$('.c-hamburger');
+  var $body =$('body');
+
+  function checkWidth(){
+    if ($window.width() < 1024) {
+    	if($body.hasClass('fixed')){
+    		$body.toggleClass('fixed');
+	      $mobile.removeClass('is-active');
+    		$header.removeClass('active');
+	      $nav.removeClass('active');
+    	}
+    	$('.site-nav').addClass('white-background');
+    }
+  };
+
+  checkWidth();
+
+  $(window).resize(checkWidth);	
 
 	var scrollStart = 0;
 	var startChange = $('#startchange');
@@ -10,18 +32,25 @@
       e.preventDefault();
         $(this).toggleClass('is-active');
         $('body').toggleClass('fixed');
-        $('site-branding').toggleClass('white-background');
         $('.site-header').toggleClass('active');
+        $('site-branding').toggleClass('white-background');
         $('.site-nav').toggleClass('active');
+        if( $('.site-nav').hasClass('white-background')){
+        	$('.site-nav').removeClass('white-background');
+      	}
     });
 
 	function addWhiteBackGroundMenu() {
+		$('site-header').addClass('white-background');
 		$('.site-branding').addClass('white-background');
+		$('.site-nav').addClass('white-background');
 		$('a.c-hamburger').addClass('white-background');
 	}
 
 	function removeWhiteBackGroundMenu() {
+		$('site-header').removeClass('white-background');
 		$('.site-branding').removeClass('white-background');
+		$('.site-nav').removeClass('white-background');
 		$('a.c-hamburger').removeClass('white-background');
 	}
 

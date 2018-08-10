e06a423fcb3b31106ab5bc0c3e1bdcd57b6f62bb
neiljoshua <neiljoshua@me.com>
2018-08-10 14:41:46 -0400

    Clean up

--- wp-content/themes/sanisidro/page.php
+++ wp-content/themes/sanisidro/page.php
@@ -18,19 +18,19 @@
 			}
 
 			if ( is_singular( 'attachment' ) ) {
-				// Parent post navigation.
+				// Parent post navigation.`
 				the_post_navigation( array(
-					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
+					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="title">%title</span>', 'Parent post link', 'twentysixteen' ),
 				) );
 			} elseif ( is_singular( 'post' ) ) {
 				// Previous/next post navigation.
 				the_post_navigation( array(
 					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
 						'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
+						'<span class="title">%title</span>',
 					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
 						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
+						'<span class="title">%title</span>',
 				) );
 			}
 			?>
